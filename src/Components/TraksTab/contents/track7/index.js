9a1e6e7001363c96c946e7d78908493cd50b6ce0
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2023-08-22 17:40:23 +0800

    feat : 更新賽道資訊
    1. 更新Track6 賽道資訊

---
+++ src/Components/TraksTab/contents/track7/index.js
@@ -0,0 +1,38 @@
+import ReactPlayer from 'react-player/youtube'
+
+const h3_style = {
+  fontWeight: 'bold',
+  fontSize: '1.5em',
+}
+const Track7_Summary_content = () => {
+  return <div></div>
+}
+
+const Track7_Benefits_content = () => {
+  return <div></div>
+}
+
+const Track7_details_content = () => {
+  return <div></div>
+}
+
+const Track7_SystemsAffected_content = () => {
+  return <div></div>
+}
+
+const Track7_Specification_content = () => {
+  return <div></div>
+}
+
+const Track7_Author_content = () => {
+  return <div></div>
+}
+
+export {
+  Track7_Summary_content,
+  Track7_Benefits_content,
+  Track7_details_content,
+  Track7_SystemsAffected_content,
+  Track7_Specification_content,
+  Track7_Author_content,
+}
