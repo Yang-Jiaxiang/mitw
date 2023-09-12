9d5a32c62b635b5ab15a3dceb028469504dbc735
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2023-09-12 22:31:20 +0800

    feat: 最新資訊頁面調整
    1. 文字靠左對其
    2. 超連結

--- src/Pages/News/Inform.js
+++ src/Pages/News/Inform.js
@@ -8,7 +8,13 @@ const newsData = [
         }}
       >
         <p>聯測督察員已開始招募，報名表單截止日期至8/20(日)</p>
-        <p>報名連結：https://forms.gle/BjpGvtuGz5gG9MUa6</p>
+        <p>
+          報名連結：
+          <br />
+          <a href="https://forms.gle/6Z6Z1Z2Z6Z6Z6Z6Z6" target="_black">
+            https://forms.gle/6Z6Z1Z2Z6Z6Z6Z6Z6
+          </a>
+        </p>
       </div>
     ),
   },
@@ -21,7 +27,7 @@ const newsData = [
         }}
       >
         <p>貴公司如欲扮演伺服器角色，請於以下兩個時間點與大會聯絡： </p>
-        <p> 1. 參與 8/24(四) 聯測預演活動，請在 8/16(三)</p>
+        <p>1. 參與 8/24(四) 聯測預演活動，請在 8/16(三)</p>
         <p>2. 參與 10/3(二)-10/6(五) 聯測活動，請在 9/26(二) 前與大會聯絡。 </p>
         <p>前與大會聯絡。 聯絡方式：misat.tw@gmail.com</p>
       </div>
