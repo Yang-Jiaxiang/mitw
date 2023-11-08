94a8943af703a041514d74e75fd040b5c5c7f7b9
楊嘉翔 <yang900412@gmail.com>
2023-11-08 16:03:41 +0800

    feat : 更新結果賽道名稱

--- src/Components/Result2023/Inform.js
+++ src/Components/Result2023/Inform.js
@@ -1,11 +1,11 @@
 const ResultTabInform = [
   { Id: 1, TabTitle: '01', ContentTitle: 'TRACK#1 - 病人基本資料' },
-  { Id: 2, TabTitle: '02', ContentTitle: 'TRACK#2 - 生理量測資訊' },
-  { Id: 3, TabTitle: '03', ContentTitle: 'TRACK#3 - 處方用藥及文件打包' },
+  { Id: 2, TabTitle: '02', ContentTitle: 'TRACK#2 - 生理量測數據' },
+  { Id: 3, TabTitle: '03', ContentTitle: 'TRACK#3 - 用藥、文件打包及服務請求' },
   { Id: 4, TabTitle: '04', ContentTitle: 'TRACK#4 - 醫學影像與病理' },
-  { Id: 5, TabTitle: '05', ContentTitle: 'TRACK#5 - 偕同照護' },
+  { Id: 5, TabTitle: '05', ContentTitle: 'TRACK#5 - 照護協調' },
   { Id: 7, TabTitle: '07', ContentTitle: 'TRACK#7 - 緊急醫療救護' },
-  { Id: 8, TabTitle: '08', ContentTitle: 'TRACK#8 - 醫療保險' },
+  { Id: 8, TabTitle: '08', ContentTitle: 'TRACK#8 - 醫療保險理賠' },
   { Id: 9, TabTitle: '09', ContentTitle: 'TRACK#9 - 遠距醫療' },
   { Id: 10, TabTitle: '10', ContentTitle: 'TRACK#10 - 癌症登記' },
 ]
